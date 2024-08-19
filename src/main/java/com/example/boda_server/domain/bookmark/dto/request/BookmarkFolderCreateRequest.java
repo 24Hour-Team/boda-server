69422546b78ae58623ced516a3fd87e7a0a2f174
EunHakKim <acky529@gmail.com>
2024-08-20 02:50:11 +0900

    BODA-30 feat: BookmarkServiceTest 단위 테스트 코드 작성

--- src/main/java/com/example/boda_server/domain/bookmark/dto/request/BookmarkFolderCreateRequest.java
+++ src/main/java/com/example/boda_server/domain/bookmark/dto/request/BookmarkFolderCreateRequest.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookmarkFolderCreateRequest {
     private String name;
+
+    public BookmarkFolderCreateRequest(String name) {
+        this.name = name;
+    }
 }
