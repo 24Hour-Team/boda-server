4899b5cb4614d9bc00ed9457cc4d6eed438c34bf
hyoom <ghkddbstj@naver.com>
2024-08-28 14:06:31 +0900

    BODA-48 feat: 사용자 정보 수정

--- src/main/java/com/example/boda_server/domain/user/controller/UserController.java
+++ src/main/java/com/example/boda_server/domain/user/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.example.boda_server.domain.user.controller;
 
 import com.example.boda_server.domain.auth.dto.CustomOAuth2User;
+import com.example.boda_server.domain.user.dto.request.UserPartialRequest;
 import com.example.boda_server.domain.user.dto.response.UserResponse;
 import com.example.boda_server.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,5 +21,10 @@ public class UserController {
 
         return ResponseEntity.ok()
                 .body(userService.getUser(customOAuth2User.getUser().getEmail()));
+    }
+
+    @PatchMapping("/user")
+    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody UserPartialRequest userPartialRequest){
+        return ResponseEntity.ok().body(userService.updateUser(customOAuth2User.getUser().getEmail(), userPartialRequest));
     }
 }
--- src/main/java/com/example/boda_server/global/config/WebConfig.java
+++ src/main/java/com/example/boda_server/global/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://accounts.kakao.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH" +
+                        "")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
